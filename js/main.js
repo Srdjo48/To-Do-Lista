46c170e09ab6d11e07d78b4f63ea1ed6d8754ac1
srdjo48 <srdjanknezevic533@gmail.com>
2022-04-10 23:20:55 +0200

    other commit

--- js/main.js
+++ js/main.js
@@ -12,52 +12,41 @@ function listLength(){
 }
 
 function createListElement() {
-	var li = document.createElement("li"); // creates an element "li"
-	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
-	ul.appendChild(li); //adds li to ul
-	input.value = ""; //Reset text input field
+	var li = document.createElement("li"); 
+	li.appendChild(document.createTextNode(input.value)); 
+	ul.appendChild(li); 
+	input.value = ""; 
 
-
-	//START STRIKETHROUGH
-	// because it's in the function, it only adds it for new items
 	function crossOut() {
 		li.classList.toggle("done");
 	}
 
 	li.addEventListener("click",crossOut);
-	//END STRIKETHROUGH
 
 
-	// START ADD DELETE BUTTON
 	var dBtn = document.createElement("button");
 	dBtn.appendChild(document.createTextNode("X"));
 	li.appendChild(dBtn);
 	dBtn.addEventListener("click", deleteListItem);
-	// END ADD DELETE BUTTON
 
-
-	//ADD CLASS DELETE (DISPLAY: NONE)
 	function deleteListItem(){
 		li.classList.add("delete")
 	}
-	//END ADD CLASS DELETE
 }
 
-
 function addListAfterClick(){
-	if (inputLength() > 0 && inputLength() < 28) { //makes sure that an empty input field doesn't create a li
+	if (inputLength() > 0 && inputLength() < 256) { 
 		createListElement();
-	} else if (inputLength() > 28) {
-		alert("Please keep items to fewer than 28 characters.");
+	} else if (inputLength() > 256) {
+		alert("Ispisati stavke na manje od 256 znakova.");
 	}
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && inputLength() < 28 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
-		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
+	if (inputLength() > 0 && inputLength() < 256 && event.which ===13) { 
 		createListElement();
-	} else if (inputLength() > 28 && event.which ===13) {
-		alert("Please keep items to fewer than 28 characters.");
+	} else if (inputLength() > 256 && event.which ===13) {
+		alert("Ispisati stavke na manje od 256 znakova.");
 	}
 }
 
